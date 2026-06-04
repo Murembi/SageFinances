25b16a3b725e48bf352bada65669801337d5a345
Chanty-z <jd2004ack@gmail.com>
2026-06-04 11:14:58 +0200

    deleted animals, added Asset class and AssetStatus enum

---
+++ src/main/java/com/example/demo/assets/AssetStatus.java
@@ -0,0 +1,8 @@
+package com.example.demo.assets;
+
+public enum AssetStatus {
+    AVAILABLE,
+    LOANED,
+    DAMAGED,
+    RETIRED
+}
--- src/main/java/com/example/demo/assets/Dog.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.assets;
-
-public class Dog {
-
-}
