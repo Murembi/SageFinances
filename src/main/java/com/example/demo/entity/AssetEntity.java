de083cf129cedafa36c2557542b26208fdbf1e73
Chanty-z <jd2004ack@gmail.com>
2026-06-05 10:56:34 +0200

    Clean up AssetEntity and improve database mappings, doing commit and push this time

--- src/main/java/com/example/demo/entity/AssetEntity.java
+++ src/main/java/com/example/demo/entity/AssetEntity.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public class AssetEntity {
 
    @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @GeneratedValue(strategy = GenerationType.IDENTITY) //yea dawg
    @Column(name = "asset_id")
    private Integer assetId;
 
