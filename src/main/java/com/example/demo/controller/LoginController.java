dcbe2069d6a522021fcee98371edf54525ec4beb
murembi <murembiwanoz64@gmail.com>
2026-06-17 15:01:27 +0200

    redirect to user dashboard

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -6,23 +6,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.example.demo.dashboard.service.UserDashboardService;
 
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
 import jakarta.servlet.http.HttpSession;
 
-import java.util.ArrayList;
-
 @Controller
 @RequestMapping("")
 public class LoginController {
 
     private final UserService userService;
+    private final UserDashboardService userDashboardService;
 
-    public LoginController(UserService userService) {
+    public LoginController(UserService userService,  UserDashboardService userDashboardService) {
         this.userService = userService;
+        this.userDashboardService = userDashboardService;
     }
+
 
     @GetMapping("/loginpage")
     public String showLoginPage() {
@@ -36,16 +38,26 @@ public class LoginController {
             HttpSession session) {
 
         User user = userService.getUserByLoginDetails(email, password);
-        session.setAttribute("user", user);        
+        session.setAttribute("user", user);
         return "redirect:/user-dashboard";
     }
 
     @GetMapping("/user-dashboard")
-    public String showUserDashboard(Model model) {
-        model.addAttribute("availableAssets", new ArrayList<>());
-        model.addAttribute("pendingLoans", new ArrayList<>());
-        model.addAttribute("loanedAssets", new ArrayList<>());
+    public String showUserDashboard(Model model, HttpSession session) {
+
+        User loggedInUser = (User) session.getAttribute("user");
+
+        if (loggedInUser == null) {
+            return "redirect:/loginpage";
+        }
+
+        Long userId = loggedInUser.getUserId();
+
+        model.addAttribute("dashboard", userDashboardService.getUserDashboard(userId));
+        model.addAttribute("availableAssets", userDashboardService.getAvailableAssets());
+        model.addAttribute("pendingLoans", userDashboardService.getMyPendingRequests(userId));
+        model.addAttribute("loanedAssets", userDashboardService.getMyLoanedAssets(userId));
+
         return "user-dashboard";
     }
-
 }
